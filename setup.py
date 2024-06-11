9cae31cb3633bc09ea9f5f2b55601c98a8b820cd
loickreienbuhl <57096911+loickreienbuhl@users.noreply.github.com>
2024-06-11 15:30:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="MotionBERT",
+    version="0.1.0",
+    author="forkedByLoic",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'tensorboardX',
+        'tqdm',
+        'easydict',
+        'prettytable',
+        'chumpy',
+        'opencv-python',
+        'imageio-ffmpeg',
+        'matplotlib==3.1.1',
+        'roma',
+        'ipdb',
+        'pytorch-metric-learning',
+        'smplx[all]'
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
